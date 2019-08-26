8fd5e31268fe21987be308eb360b9f1c53567d72
Konstantin Kowalski <konstantin@bstocksolutions.com>
2019-08-26 07:55:35 -0400

    improve drawing goal; begin ball logic

--- test.py
+++ test.py
@@ -2,34 +2,121 @@ import turtle as t
 import time
 
 t.speed('fastest')
+t.delay(0)
+t.hideturtle()
+t.tracer(False)
 
 def drawball(x, y, distance=20):
+   '''
    t.tracer(15, 0)
    t.forward(x)
    t.right(90)
    t.forward(y)
    t.clear()
+   '''
+   t.pencolor("white")
+   t.setpos(x, y)
+   t.color("black")
 
-   t.tracer(7, 0)
+   #t.tracer(7, 0)
    for i in range(0, 3):
       t.forward(distance)
       t.right(90)
    t.forward(distance)
 
+def draw_goal(y_start, is_left):
+
+   t.seth(0)
+
+   #t.reset()
+   x = -300 if is_left else 300
+   y = y_start
+   #t.pencolor("white")
+   t.penup()
+   t.setpos(x, y)
+   t.pendown()
+   #t.color("black")
+
+   #t.tracer(7, 0)
+   t.forward(10)
+   t.right(90)
+   t.forward(50)
+   t.right(90)
+   t.forward(10)
+   t.right(90)
+   t.forward(50)
+
+i = 0
+def perform_logic(x, y, goal1_y, goal2_y, angle):
+   '''
+   x/y = x and y of the ball
+   angle = ball direction
+   goal1_y/goal2_y = goal position
+   '''
+   global i
+   i+= 1
+   if i % 40 == 0:
+      print(x,y,goal1_y, goal2_y, angle)
+      #time.sleep(2)
+
+   if x > 300:
+      print('switching dir', x,y,goal1_y, goal2_y, angle)
+      angle = 260
+   elif x <= -300:
+      print('switching dir', x,y,goal1_y, goal2_y, angle)
+      angle = 90
+
+   if angle >= 0 and angle <= 180:
+      #print('forward', angle)
+      x += 2
+      y += 2
+   elif angle >= 180:
+      #print('backward', angle)
+      x -= 2
+      y -= 2
+
+   return x, y, goal1_y, goal2_y, angle
+
 x = 0
 y = 0
+goal1_y = 0
+goal2_y = 0
+angle = 0
 def run():
-   global x
-   global y
-   x += 1
-   y += 1
+   global x, y, goal1_y, goal2_y, angle
+
+   x, y, goal1_y, goal2_y, angle = perform_logic(x, y, goal1_y, goal2_y, angle)
+
    drawball(x, y)
 
+   draw_goal(goal1_y, False)
+   draw_goal(goal2_y, True)
 
+def on_key_down():
+   global goal1_y
+   goal1_y -= 5
+def on_key_up():
+   global goal1_y
+   goal1_y += 5
+def on_key_w():
+   global goal2_y
+   goal2_y += 5
+def on_key_s():
+   global goal2_y
+   goal2_y -= 5
+t.onkey(on_key_up, 'Up')
+t.onkey(on_key_down, 'Down')
+t.onkey(on_key_w, 'w')
+t.onkey(on_key_s, 's')
+
+t.listen()
 while True:
    t.reset()
    t.hideturtle()
    run()
-   time.sleep(0.1)
+   t.update()
+   time.sleep(0.05)
    t.clear()
+   #t.mainloop()
+
 
